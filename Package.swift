1578289b6aea74bc0eac64e9974a1bf7eacccc94
Julien Pouget <julien.pouget@me.com>
2024-03-11 16:32:33 +0100

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "RichTextRow",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(name: "RichTextRow", targets: ["RichTextRow"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/xmartlabs/Eureka.git", from: "5.4.0")
+    ],
+    targets: [
+        .target(
+            name: "RichTextRow",
+            dependencies: ["Eureka"],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "RichTextRowTests",
+            dependencies: ["RichTextRow"],
+            path: "RichTextRowTests"
+        )
+    ]
+)
